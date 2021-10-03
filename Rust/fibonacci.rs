3a0e2aa9b94d5e40885bac6c794fa60835540564
Atsushi <88151256+atshushi@users.noreply.github.com>
2021-10-03 14:53:38 -0300

    Update fibonacci.rs

--- Rust/fibonacci.rs
+++ Rust/fibonacci.rs
@@ -1,11 +1,7 @@
-fn main() {
-  fn fib(n: usize) -> usize {
-    match n {
-      0 => 0,
-      1 => 1,
-      _ => fib(n - 1) + fib(n - 2)
-    }
+fn fib(n: usize) -> usize {
+  match n {
+    0 => 0,
+    1 => 1,
+    _ => fib(n - 1) + fib(n - 2)
   }
-
-  fib(10); // returns 55
 }
